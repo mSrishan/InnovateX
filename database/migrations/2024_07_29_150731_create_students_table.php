b9d95463676a406a89fa238e369005c9f831ab40
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-07-29 23:44:16 +0530

    update

--- database/migrations/2024_07_29_150731_create_students_table.php
+++ database/migrations/2024_07_29_150731_create_students_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->timestamps(); //created_at, updated_at
         });
     }
 
